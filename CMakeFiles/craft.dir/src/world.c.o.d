9982972066d0c249b5702c0390b58adc7cd3065f
CalmDownEthan <1745612@qq.com>
2026-04-18 21:57:15 +0800

    make win craft

--- CMakeFiles/craft.dir/src/world.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/craft.dir/src/world.c.o: \
- /home/lighthouse/Craft-master/src/world.c /usr/include/stdc-predef.h \
- /home/lighthouse/Craft-master/src/config.h \
- /home/lighthouse/Craft-master/deps/noise/noise.h \
- /home/lighthouse/Craft-master/src/world.h
