e77da4106c91242b5059d7256d7b94556065e2c4
CalmDownEthan <1745612@qq.com>
2026-04-18 17:26:34 +0800

    code

---
+++ CMakeFiles/craft.dir/src/auth.c.o.d
@@ -0,0 +1,37 @@
+CMakeFiles/craft.dir/src/auth.c.o: \
+ /home/lighthouse/Craft-master/src/auth.c /usr/include/stdc-predef.h \
+ /usr/include/curl/curl.h /usr/include/curl/curlver.h \
+ /usr/include/curl/system.h /usr/include/sys/types.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/time_t.h /usr/include/bits/types/timer_t.h \
+ /usr/lib/gcc/x86_64-OpenCloudOS-linux/12/include/stddef.h \
+ /usr/include/bits/stdint-intn.h /usr/include/sys/socket.h \
+ /usr/include/bits/types/struct_iovec.h /usr/include/bits/socket.h \
+ /usr/include/bits/socket_type.h /usr/include/bits/sockaddr.h \
+ /usr/include/bits/socket-constants.h /usr/include/stdio.h \
+ /usr/include/bits/libc-header-start.h \
+ /usr/lib/gcc/x86_64-OpenCloudOS-linux/12/include/stdarg.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h /usr/include/bits/stdio.h \
+ /usr/lib/gcc/x86_64-OpenCloudOS-linux/12/include/limits.h \
+ /usr/lib/gcc/x86_64-OpenCloudOS-linux/12/include/syslimits.h \
+ /usr/include/limits.h /usr/include/time.h /usr/include/bits/time.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/struct_tm.h \
+ /usr/include/sys/time.h /usr/include/bits/types/struct_timeval.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/types/sigset_t.h /usr/include/bits/types/__sigset_t.h \
+ /usr/include/curl/easy.h /usr/include/curl/multi.h \
+ /usr/include/curl/curl.h /usr/include/curl/urlapi.h \
+ /usr/include/curl/options.h /usr/include/curl/header.h \
+ /usr/include/curl/websockets.h /usr/include/curl/typecheck-gcc.h \
+ /usr/include/stdlib.h /usr/include/bits/stdlib-bsearch.h \
+ /usr/include/bits/stdlib-float.h /usr/include/string.h \
+ /home/lighthouse/Craft-master/src/auth.h
---
+++ CMakeFiles/craft.dir/src/matrix.c.o.d
@@ -0,0 +1,23 @@
+CMakeFiles/craft.dir/src/matrix.c.o: \
+ /home/lighthouse/Craft-master/src/matrix.c /usr/include/stdc-predef.h \
+ /usr/include/math.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/math-vector.h \
+ /usr/include/bits/libm-simd-decl-stubs.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h /usr/include/bits/flt-eval-method.h \
+ /usr/include/bits/fp-logb.h /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /home/lighthouse/Craft-master/src/config.h \
+ /home/lighthouse/Craft-master/src/matrix.h \
+ /home/lighthouse/Craft-master/src/util.h \
+ /home/lighthouse/Craft-master/deps/glew/include/GL/glew.h \
+ /usr/lib/gcc/x86_64-OpenCloudOS-linux/12/include/stddef.h \
+ /usr/lib/gcc/x86_64-OpenCloudOS-linux/12/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/GL/glu.h /usr/include/GL/gl.h \
+ /home/lighthouse/Craft-master/deps/glfw/include/GLFW/glfw3.h
